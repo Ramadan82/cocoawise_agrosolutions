43d2a07cb131e20d24dda138b45b37e319f38517
ramadan82 <ramadan.isa@gmail.com>
2021-03-08 22:27:02 +0000

    home page1

--- src/components/pages/HomePage/Data.js
+++ src/components/pages/HomePage/Data.js
@@ -2,8 +2,8 @@ export const homeObjOne = {
   lightBg: false,
   lightText: true,
   lightTextDesc: true,
-  topLine: "Exclusive Access",
-  headline: "Unlimited Transactions with zero fees",
+  topLine: "Welcome to Cocoawise Agro Solutions",
+  headline: "The Ultimate in Cocoa Busines",
   description:
     "Get access to our exclusive diamond card that allows you to  send unlimited transactions without getting charged any fees",
   buttonLabel: "Get Started",
